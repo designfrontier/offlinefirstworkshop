95e4a1c23f39e9542a4bb5a24f4bcf25522f1f8f
Daniel Sellers <d.sellers@ldschurch.org>
2014-10-13 21:50:49 -0600

    changed some things to be more event driven

--- public/scripts/libs/logic.js
+++ public/scripts/libs/logic.js
@@ -12,7 +12,9 @@
                 //  |A| = a(ei - fh) - b(di - fg) + c(dh - eg)
                 //  !! converts to boolean
                 //  true means a win
-                return !!(moves[0][0](moves[1][1] * moves[2][2] - moves[1][2] * moves[2][1]) - moves[0][1] (moves[1][0] * moves[2][2] - moves[1][2] * moves[2][0]) + moves[0][2](moves[1][0] * moves[2][1] - moves[1][1] * moves[2][0]));
+                if(!!(moves[0][0](moves[1][1] * moves[2][2] - moves[1][2] * moves[2][1]) - moves[0][1] (moves[1][0] * moves[2][2] - moves[1][2] * moves[2][0]) + moves[0][2](moves[1][0] * moves[2][1] - moves[1][1] * moves[2][0]))){
+                    obj.emit('game:win', obj.id);
+                }
             };
 
         if( typeof obj.id === 'undefined' ){
