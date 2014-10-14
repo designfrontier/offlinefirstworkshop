3d34db1234688c499a1fad29cabeb5930cb52045
Daniel Sellers <d.sellers@ldschurch.org>
2014-10-14 11:35:20 -0600

    got the appcache manifest working and everything looks pretty sweet

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var gulp = require('gulp')
     , concat = require('gulp-concat')
     , uglify = require('gulp-uglify');
 
-gulp.task('default', ['buildJS'], function(){
+gulp.task('default', ['buildJS', 'buildStyles'], function(){
     //concat and uglify
 });
 
-gulp.task('build', ['buildJS', 'manifest'], function(){
+gulp.task('build', ['buildJS', 'buildStyles', 'manifest'], function(){
     //combine the js files
     //update the appcache manifest
 });
@@ -16,7 +16,6 @@ gulp.task('manifest', function(){
     gulp.src(['public/built/*'])
         .pipe(manifest({
           hash: true,
-          preferOnline: true,
           network: ['http://*', 'https://*', '*'],
           filename: 'manifest.appcache',
           exclude: 'manifest.appcache'
@@ -32,4 +31,9 @@ gulp.task('buildJS', function(){
         .pipe(concat('app.js'))
         .pipe(uglify())
         .pipe(gulp.dest('public/built'));
+});
+
+gulp.task('buildStyles', function(){
+    gulp.src('public/styles/style.css')
+        .pipe(gulp.dest('public/built/'));
 });
