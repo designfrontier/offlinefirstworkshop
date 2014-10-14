a9f110ab5b874e67100cee6223e3fd6bb2f5c92a
Daniel Sellers <d.sellers@ldschurch.org>
2014-10-14 11:11:33 -0600

    cleaning up the gulpfile and getting builds working

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,35 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+    , manifest = require('gulp-manifest')
+    , concat = require('gulp-concat')
+    , uglify = require('gulp-uglify');
 
-gulp.task('default', function(){
-
+gulp.task('default', ['buildJS'], function(){
+    //concat and uglify
 });
 
-gulp.task('build', function(){
-    //combine the files
+gulp.task('build', ['buildJS', 'manifest'], function(){
+    //combine the js files
     //update the appcache manifest
-    //move into a build folder
-    
+});
+
+gulp.task('manifest', function(){
+    gulp.src(['public/built/*'])
+        .pipe(manifest({
+          hash: true,
+          preferOnline: true,
+          network: ['http://*', 'https://*', '*'],
+          filename: 'manifest.appcache',
+          exclude: 'manifest.appcache'
+         }))
+        .pipe(gulp.dest('public/built'));
+});
+
+gulp.task('buildJS', function(){
+    gulp.src([
+                'public/scripts/app.js'
+                , 'public/scripts/components/**/*.js'
+            ])
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/built'));
 });
