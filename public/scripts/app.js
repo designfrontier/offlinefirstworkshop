d5f7872a2eacc5b0833ee90316326c0facdc2bbd
Daniel Sellers <d.sellers@ldschurch.org>
2014-10-14 05:55:35 -0600

    working on getting everything wired up. have the game initializing with events and DOM elements. Lots of cleanup I would like to do if I can get to it before the presentation.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,49 @@
 (function(){
+    var boxes = document.querySelectorAll('.game-board--box');
+
     window.ticTac = {};
 
+    window.ticTac.game = action.eventMe({
+        init: function(){
+            var that = this;
+
+            that.listen('game:win', function(id){
+                console.log(id + 'wins!');
+
+                that.emit('game:reset');
+            });
+
+            that.listen('game:start', function(){
+                that.active = 'x';
+            });
+
+            that.listen('game:move:x', function(){
+                that.active = 'o';
+            });
+
+            that.listen('game:move:o', function(){
+                that.active = 'x';
+            });
+        }
+    });
+
+    [].forEach.call(boxes, function(box){
+        box.addEventListener('click', function(){
+            box.textContent = ticTac.game.active;
+            action.emit('game:move:' + window.ticTac.game.active, {row: box.attributes['data-row'], col: box.attributes['data-col']});
+        });
+    });
+
+    document.querySelector('.start-game--button').addEventListener('click', function(){
+        //set up the game
+        ticTac.active = 'o';
+        ticTac.players = [];
+
+        ticTac.players.push(ticTac.createPlayer({id:'x'}));
+        ticTac.players.push(ticTac.createPlayer({id:'o'}));
+
+        ticTac.game.emit('game:start');
+
+        document.querySelector('.start-game').classList.add('hidden');
+    });
 })()
--- public/scripts/libs/logic.js
+++ public/scripts/libs/logic.js
@@ -1,34 +1,37 @@
 (function (exportMe) {
     var createPlayer = function (objIn) {
         var obj = action.eventMe(objIn || {})
-            , moves = [
-                [0,0,0]
-                , [0,0,0]
-                , [0,0,0]
-            ]
+            , moves = [[0,0,0], [0,0,0], [0,0,0]]
 
             , checkWin = function () {
                 //Matrix determinant... math!
                 //  |A| = a(ei - fh) - b(di - fg) + c(dh - eg)
                 //  !! converts to boolean
                 //  true means a win
-                if(!!(moves[0][0](moves[1][1] * moves[2][2] - moves[1][2] * moves[2][1]) - moves[0][1] (moves[1][0] * moves[2][2] - moves[1][2] * moves[2][0]) + moves[0][2](moves[1][0] * moves[2][1] - moves[1][1] * moves[2][0]))){
+                if(!!(moves[0][0] * ((moves[1][1] * moves[2][2]) - (moves[1][2] * moves[2][1])) - moves[0][1] * ((moves[1][0] * moves[2][2]) - (moves[1][2] * moves[2][0])) + moves[0][2] * ((moves[1][0] * moves[2][1]) - (moves[1][1] * moves[2][0])))){
                     obj.emit('game:win', obj.id);
                 }
+            }
+
+            , setMove = function(objIn){
+                moves[objIn.row][objIn.col] = 1;
+                console.log(objIn);
+                checkWin();
             };
 
         if( typeof obj.id === 'undefined' ){
-            id = 'x';
+            obj.id = 'x';
         }
 
-        obj.setMove = function(row, col){
-            moves[row][col] = 1;
-            checkWin();
-        };
+        obj.listen('game:move:' + obj.id, setMove);
+
+        obj.listen('game:reset', function() {
+            moves = [[0,0,0], [0,0,0], [0,0,0]];
+        });
 
         return obj;
     };
 
-    exportMe = {createPlayer: createPlayer};
+    exportMe['createPlayer'] = createPlayer;
 
 })((typeof module !== 'undefined' && module.exports) || window.ticTac)
